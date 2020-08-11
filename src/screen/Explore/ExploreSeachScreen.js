d7c0d8734929b2f5fbe6ca9172e99e4272431e2b
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-11 12:55:52 +0700

    add explore search

--- src/screen/Explore/ExploreSeachScreen.js
+++ src/screen/Explore/ExploreSeachScreen.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
+
 import MainHeader from '../../component/Header/MainHeader';
 import Screen from '../../component/Screen';
 import { View, TextInput } from 'react-native';
 import Colors from '../../utils/color';
+import { SEARCH_URL } from '../../utils/api';
+import SearchResultList from '../../component/Search/SearchResult';
 
 const ExploreSearchScreen = () => {
+  const [data, setData] = useState([]);
+
+  const getSearchData = (query) => {
+    if (query === '') {
+      setData([]);
+    } else {
+      Axios.get(SEARCH_URL(query))
+        .then((res) => {
+          setData(res.data.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <>
-      <MainHeader centerComponent={() => <Search />} withBack />
-      <Screen />
+      <MainHeader centerComponent={() => <Search onChange={getSearchData} />} withBack />
+      <Screen>
+        <SearchResultList result={data} />
+      </Screen>
     </>
   );
 };
 
-const Search = () => {
+const Search = ({ onChange }) => {
   return (
     <View
       style={{ backgroundColor: Colors['dark-4'], width: '100%', padding: 10, borderRadius: 4 }}
@@ -25,6 +45,12 @@ const Search = () => {
           fontSize: 18,
           padding: 0,
         }}
+        autoCorrect={false}
+        autoCapitalize={'none'}
+        selectionColor={Colors['white-1']}
+        onChangeText={(text) => onChange(text.toLowerCase())}
+        autoFocus={true}
+        returnKeyType={'search'}
       />
     </View>
   );

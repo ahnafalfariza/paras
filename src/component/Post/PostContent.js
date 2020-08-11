c4193e1a91c896c21203b2211aeabbaa860e166b
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-11 12:56:47 +0700

    add custom button next prev and pagination

--- src/component/Post/PostContent.js
+++ src/component/Post/PostContent.js
@@ -6,25 +6,43 @@ import LinkContent from './Content/LinkContent';
 import ImageContent from './Content/ImageContent';
 import TextContent from './Content/TextContent';
 import { SCREEN_WIDTH } from '../../utils/constant';
+import { SvgXml } from 'react-native-svg';
+import Colors from '../../utils/color';
+import assetSvg from '../../assets/svg/svg';
 
 const PostContent = ({ contentList }) => {
   return (
     <View style={{ height: SCREEN_WIDTH - 32 }}>
       <Swiper
         showsButtons={true}
+        nextButton={
+          <SvgXml xml={assetSvg.postContent.next} width="28" height="28" fill={Colors['white-1']} />
+        }
+        prevButton={
+          <SvgXml xml={assetSvg.postContent.prev} width="28" height="28" fill={Colors['white-1']} />
+        }
         loop={false}
         showsPagination={true}
-        renderPagination={(index, total, swiper) => {
-          // console.log('index', index, total);
-          // console.log('total', total);
+        dotStyle={{
+          width: 10,
+          height: 2,
+          borderRadius: 0,
+          backgroundColor: Colors['white-2'],
         }}
+        activeDotStyle={{
+          width: 12,
+          height: 3,
+          borderRadius: 0,
+          backgroundColor: Colors['white-1'],
+        }}
+        paginationStyle={{ position: 'absolute', bottom: -20 }}
       >
         {contentList.map(({ type, body }, index) => {
           if (type === 'text') {
             return <TextContent key={index} body={body} />;
           } else if (type === 'img') {
             return <ImageContent key={index} body={body} />;
-          } else {
+          } else if (type === 'url') {
             return <LinkContent key={index} body={body} />;
           }
         })}

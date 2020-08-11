9e0932e548d3c9f1de5cc092fa48fe94de27b2b5
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-11 12:51:17 +0700

    add search and memorygrants api

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,4 +3,8 @@ export const BASE_URL = 'https://api-dev.paras.id/';
 export const EXPLORE_URL = (page, limit = 10) =>
   `${BASE_URL}posts?__skip=${(page - 1) * limit}&__limit=${limit}&__sort=-createdAt`;
 
+export const MEMORY_GRANTS = `${BASE_URL}grants?isActive=true`;
+
+export const SEARCH_URL = (query) => `${BASE_URL}search?id__re=${query}`;
+
 export const PROFILE_URL = () => '';

14a579ef87fd22149823e434728b391aea2f10b0
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-11 12:43:27 +0700

    add platform constant

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,7 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
 
 export const SCREEN_HEIGHT = Dimensions.get('window').height;
 export const SCREEN_WIDTH = Dimensions.get('window').width;
+
+export const isAndroid = Platform.OS === 'android';
+export const isIOS = Platform.OS === 'ios';

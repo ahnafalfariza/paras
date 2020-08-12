f14c63e15655e8d480a1f3763bb2e218c542101c
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-12 11:03:46 +0700

    modify tab navigator profile icon

--- src/navigator/TabNavigator.js
+++ src/navigator/TabNavigator.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { View } from 'react-native';
+import { useSelector } from 'react-redux';
+import FastImage from 'react-native-fast-image';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SvgXml } from 'react-native-svg';
 
@@ -10,32 +13,34 @@ import ExploreNavigator from './ExploreNavigator';
 
 import Colors from '../utils/color';
 import assetSvg from '../assets/svg/svg';
+import { getImageUrl } from '../utils/image';
 
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
+  const user = useSelector((state) => state.user.user);
   return (
     <Tab.Navigator
       initialRouteName={'Home'}
       tabBarOptions={tabBarOption}
-      screenOptions={({ route }) => ({ tabBarIcon: ({ color }) => tabBarIcon(color, route) })}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => tabBarIcon(color, route, user),
+      })}
     >
       <Tab.Screen name="Home" component={HomeNavigator} />
       <Tab.Screen name="Explore" component={ExploreNavigator} />
-      <Tab.Screen
-        name="NewPost"
-        component={NewPostScreen}
-        listeners={({ navigation }) => ({
-          tabPress: (e) => {
-            e.preventDefault();
-            navigation.navigate('New Post');
-          },
-        })}
-      />
+      <Tab.Screen name="NewPost" component={NewPostScreen} listeners={newPostListener} />
       <Tab.Screen name="Wallet" component={WalletNavigator} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 };
+
+const newPostListener = ({ navigation }) => ({
+  tabPress: (e) => {
+    e.preventDefault();
+    navigation.navigate('New Post');
+  },
+});
 
 const tabBarOption = {
   activeTintColor: Colors['primary-5'],
@@ -45,10 +50,22 @@ const tabBarOption = {
   showLabel: false,
 };
 
-const tabBarIcon = (color, route) => {
+const tabBarIcon = (color, route, user) => {
   const { name } = route;
   const xml = assetSvg.bottomTab[name];
-  return <SvgXml xml={xml} width="60%" height="60%" fill={color} />;
+
+  if (name === 'Profile') {
+    return (
+      <View style={{ borderWidth: 3, borderRadius: 20, borderColor: color }}>
+        <FastImage
+          source={{ uri: getImageUrl(user.imgAvatar) }}
+          style={{ width: 32, height: 32 }}
+        />
+      </View>
+    );
+  }
+
+  return <SvgXml xml={xml} width="30" height="30" fill={color} />;
 };
 
 export default TabNavigator;

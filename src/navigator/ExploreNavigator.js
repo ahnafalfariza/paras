a18ce06ea42f65b7d0399c9dcdc3a516f1e8bb4e
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-12 15:49:09 +0700

    add screen to navigator

--- src/navigator/ExploreNavigator.js
+++ src/navigator/ExploreNavigator.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import ExploreSearchScreen from '../screen/Explore/ExploreSeachScreen';
 import ExploreScreen from '../screen/Explore/ExploreScreen';
+import MementoScreen from '../screen/Common/MementoScreen';
+import UserScreen from '../screen/Common/UserScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +12,8 @@ const ExploreNavigator = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Explore" component={ExploreScreen} />
       <Stack.Screen name="Search" component={ExploreSearchScreen} />
+      <Stack.Screen name="Memento" component={MementoScreen} />
+      <Stack.Screen name="UserProfile" component={UserScreen} />
     </Stack.Navigator>
   );
 };

43850ca6e5c01fc6370a85eba5858c3f86313b91
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-18 12:09:16 +0700

    refactor profile screen

--- src/screen/ProfileScreen.js
+++ src/screen/ProfileScreen.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
+import { SvgXml } from 'react-native-svg';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { connect } from 'react-redux';
 
 import Screen from '../component/Common/Screen';
 import MainHeader from '../component/Header/MainHeader';
 import Profile from '../component/Profile/Profile';
-
-const user = {
-  id: 'zilalvs.testnet',
-  imgAvatar: {
-    url: 'QmUMKPXyPogETNyJvZCPdgd6qDSeEdLWK5nmdZtbkzGVra',
-    type: 'ipfs',
-  },
-  bio: 'written thoughts; constantly educating myself.',
-  createdAt: '1593851018660737775',
-};
+import assetSvg from '../assets/svg/svg';
+import Colors from '../utils/color';
 
 class ProfileScreen extends Component {
   render() {
     return (
       <>
-        <MainHeader title={'Profile'} />
+        <MainHeader
+          title={'Profile'}
+          rightComponent={() => (
+            <TouchableWithoutFeedback onPress={() => console.log('more profile')}>
+              <View style={{ paddingRight: 4 }}>
+                <SvgXml
+                  xml={assetSvg.common.more}
+                  width="28"
+                  height="28"
+                  style={{ justifyContent: 'flex-end' }}
+                  fill={Colors['white-1']}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+          )}
+        />
         <Screen>
-          <Profile data={user} />
+          <Profile data={this.props.userData} />
         </Screen>
       </>
     );
   }
 }
 
-export default ProfileScreen;
+const mapStateToProps = (state) => ({
+  userData: state.user.user,
+});
+
+export default connect(mapStateToProps)(ProfileScreen);

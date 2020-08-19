85e60052ab9d3c74929d382feb046e42dc926a76
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-19 15:32:05 +0700

    add RoutesName.js

---
+++ src/utils/RoutesName.js
@@ -0,0 +1,22 @@
+export default {
+  Tab: 'HomeTab',
+
+  HomePicks: 'HomePicks',
+  HomeFollowing: 'HomeFollowing',
+
+  Explore: 'Explore',
+  ExploreSerach: 'ExploreSearch',
+  Memento: 'Memento',
+  UserProfile: 'UserProfile',
+
+  Wallet: 'Wallet',
+  WalletHistory: 'WalletHistory',
+  WalletTransaction: 'WalletTransaction',
+
+  Landing: 'Landing',
+  Login: 'Login',
+  Registration: 'Registration',
+  Verification: 'Verification',
+  SeedPassword: 'SeedPassword',
+  SeedConfirmation: 'SeedConfirmation',
+};

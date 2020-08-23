6ebcf9b3fa295f346111f3108fda507ce024c6e5
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-08-23 22:01:07 +0700

    setup default authorization in appnavigator

--- src/navigator/AppNavigator.js
+++ src/navigator/AppNavigator.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
+import Axios from 'axios';
 
 import NewPostScreen from '../screen/NewPost/NewPostScreen';
 import TabNavigator from './TabNavigator';
 import LandingNavigator from './LandingNavigator';
 import AuthNavigator from './AuthNavigator';
+import { VERIFY_TOKEN } from '../utils/api';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = ({ isLoggedIn }) => {
+  const token = useSelector((state) => state.user.token);
+  const [tokenLoaded, setTokenLoaded] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      Axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+      Axios.post(VERIFY_TOKEN)
+        .then(() => setTokenLoaded(true))
+        .catch((err) => console.log(err));
+    } else {
+      setTokenLoaded(true);
+    }
+  }, [token]);
+
+  if (!tokenLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {!isLoggedIn ? (
           <>
-            <Stack.Screen name="LandingNavigator" component={LandingNavigator} />
+            {/* <Stack.Screen name="LandingNavigator" component={LandingNavigator} /> */}
             <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
           </>
         ) : (

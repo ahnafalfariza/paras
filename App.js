c8053041cc5e6e1bf9cae656343510288427c166
hdriqi <riqi.asdf@gmail.com>
2020-09-11 21:33:24 +0700

    register device token on app open

--- App.js
+++ App.js
@@ -1,47 +1,57 @@
-import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/es/integration/react';
+import React, { useEffect } from 'react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/es/integration/react'
 
-import configureStore from './src/store';
-import AppNavigator from './src/navigator/AppNavigator';
-import { fcmService } from './src/services/FCMService';
-import { localNotificationService } from './src/services/LocalNotificationService';
+import configureStore from './src/store'
+import AppNavigator from './src/navigator/AppNavigator'
+import { fcmService } from './src/services/FCMService'
+import { localNotificationService } from './src/services/LocalNotificationService'
+import Axios from 'axios'
+import { REGISTER_DEVICE } from './src/utils/api'
+import { Platform } from 'react-native'
 
-const { persistor, store } = configureStore();
+const { persistor, store } = configureStore()
 
 const App = () => {
   useEffect(() => {
-    fcmService.registerAppWithFCM;
-    fcmService.register(onRegister, onNotification, onOpenNotification);
-    localNotificationService.configure(onOpenNotification);
+    fcmService.registerAppWithFCM
+    fcmService.register(onRegister, onNotification, onOpenNotification)
+    localNotificationService.configure(onOpenNotification)
 
     function onRegister(token) {
-      console.log('[App] onRegister: ', token);
+      console.log('[App] onRegister: ', token)
+      Axios.post(REGISTER_DEVICE, { deviceId: token, type: Platform.OS })
+        .then((res) => {
+          console.log(res.data.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
       //alert("[App] onRegister: ", token)
     }
 
     function onNotification(notify) {
-      console.log('[App] onNotification: ', notify);
+      console.log('[App] onNotification: ', notify)
       //alert("[App] onNotification: ", notify)
       const options = {
         soundName: 'default',
         playSound: true,
-      };
-      localNotificationService.showNotification(0, notify.title, notify.body, notify, options);
+      }
+      localNotificationService.showNotification(0, notify.title, notify.body, notify, options)
     }
 
     function onOpenNotification(notify) {
-      console.log('[App] onOpenNotification: ', notify);
+      console.log('[App] onOpenNotification: ', notify)
       //alert("Open Notification: " + notify.body)
     }
 
     return () => {
-      console.log('[App] unRegister');
+      console.log('[App] unRegister')
       //alert("[App] unRegister")
-      fcmService.unRegister();
-      localNotificationService.unRegister();
-    };
-  }, []);
+      fcmService.unRegister()
+      localNotificationService.unRegister()
+    }
+  }, [])
 
   return (
     <Provider store={store}>
@@ -49,7 +59,7 @@ const App = () => {
         <AppNavigator />
       </PersistGate>
     </Provider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,50 +1,51 @@
-import { postLimit } from './constant';
+import { postLimit } from './constant'
 
-const BASE_URL = 'https://api.dev.paras.id';
+const BASE_URL = 'https://api.dev.paras.id'
 
 export const EXPLORE_URL = (page) =>
-  `${BASE_URL}/posts?__skip=${(page - 1) * postLimit}&__limit=${postLimit}&__sort=-createdAt`;
-export const MEMORY_GRANTS_URL = `${BASE_URL}/grants?isActive=true`;
-export const SEARCH_URL = (query) => `${BASE_URL}/search?id__re=${query}`;
+  `${BASE_URL}/posts?__skip=${(page - 1) * postLimit}&__limit=${postLimit}&__sort=-createdAt`
+export const MEMORY_GRANTS_URL = `${BASE_URL}/grants?isActive=true`
+export const SEARCH_URL = (query) => `${BASE_URL}/search?id__re=${query}`
 
-export const PROFILE_URL = (id) => `${BASE_URL}/users?id=${id}`;
+export const PROFILE_URL = (id) => `${BASE_URL}/users?id=${id}`
 export const PROFILE_POST_URL = (id, page) =>
   `${BASE_URL}/posts?owner=${id}&__skip=${
-    (page - 1) * postLimit
-  }&__limit=${postLimit}&__sort=-createdAt`;
+  (page - 1) * postLimit
+  }&__limit=${postLimit}&__sort=-createdAt`
 
-export const MEMENTO_URL = (id) => `${BASE_URL}/mementos?id=${id}`;
+export const MEMENTO_URL = (id) => `${BASE_URL}/mementos?id=${id}`
 export const MEMENTO_POST_URL = (id, page) =>
   `${BASE_URL}/posts?mementoId=${id}&__skip=${
-    (page - 1) * postLimit
-  }&__limit=${postLimit}&__sort=-createdAt`;
+  (page - 1) * postLimit
+  }&__limit=${postLimit}&__sort=-createdAt`
 
 //auth
-export const CREATE_USER = `${BASE_URL}/register`;
-export const LOGIN = `${BASE_URL}/login`;
-export const VERIFY_USER = `${BASE_URL}/register/confirm`;
-export const VERIFY_TOKEN = `${BASE_URL}/verify`;
-export const FOLLOWING_LIST = `${BASE_URL}/follow`;
+export const CREATE_USER = `${BASE_URL}/register`
+export const LOGIN = `${BASE_URL}/login`
+export const VERIFY_USER = `${BASE_URL}/register/confirm`
+export const VERIFY_TOKEN = `${BASE_URL}/verify`
+export const FOLLOWING_LIST = `${BASE_URL}/follow`
+export const REGISTER_DEVICE = `${BASE_URL}/register/device`
 
-export const HOME_FEED = `${BASE_URL}/feeds`;
-export const HOME_PICKS = `${BASE_URL}/timelines?id=editorsPick`;
+export const HOME_FEED = `${BASE_URL}/feeds`
+export const HOME_PICKS = `${BASE_URL}/timelines?id=editorsPick`
 
 //wallet
-export const WALLET_BALANCE = (user) => `${BASE_URL}/balances/${user}`;
-export const WALLET_HISTORY = `${BASE_URL}/transactions`;
-export const SEARCH_USER_SEND = (query) => `${BASE_URL}/users?id__re=${query}`;
+export const WALLET_BALANCE = (user) => `${BASE_URL}/balances/${user}`
+export const WALLET_HISTORY = `${BASE_URL}/transactions`
+export const SEARCH_USER_SEND = (query) => `${BASE_URL}/users?id__re=${query}`
 
 //newPost
-export const USER_MEMENTO = (user) => `${BASE_URL}/mementos?owner=${user}`;
-export const SEARCH_MEMENTO = (query) => `${BASE_URL}/mementos?id__re=${query}`;
-export const CREATE_MEMENTO = `${BASE_URL}/mementos`;
-export const CREATE_POST = `${BASE_URL}/posts`;
-export const DELETE_POST = (postId) => `${BASE_URL}/posts/${postId}`;
-export const EDIT_POST = (postId) => `${BASE_URL}/posts/${postId}`;
-export const META_URL = (link) => `${BASE_URL}/metaget?link=${link}`;
-export const REDACT_POST = (postId) => `${BASE_URL}/posts/${postId}/redact`;
+export const USER_MEMENTO = (user) => `${BASE_URL}/mementos?owner=${user}`
+export const SEARCH_MEMENTO = (query) => `${BASE_URL}/mementos?id__re=${query}`
+export const CREATE_MEMENTO = `${BASE_URL}/mementos`
+export const CREATE_POST = `${BASE_URL}/posts`
+export const DELETE_POST = (postId) => `${BASE_URL}/posts/${postId}`
+export const EDIT_POST = (postId) => `${BASE_URL}/posts/${postId}`
+export const META_URL = (link) => `${BASE_URL}/metaget?link=${link}`
+export const REDACT_POST = (postId) => `${BASE_URL}/posts/${postId}/redact`
 
 //comment
-export const COMMENT = (postId) => `${BASE_URL}/comments?postId=${postId}`;
-export const POST_COMMENT = `${BASE_URL}/comments`;
-export const DELETE_COMMENT = (commentId) => `${BASE_URL}/comments/${commentId}`;
+export const COMMENT = (postId) => `${BASE_URL}/comments?postId=${postId}`
+export const POST_COMMENT = `${BASE_URL}/comments`
+export const DELETE_COMMENT = (commentId) => `${BASE_URL}/comments/${commentId}`

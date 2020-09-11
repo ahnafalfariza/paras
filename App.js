e15861b99af75694126b185b90911b55f227056e
hdriqi <riqi.asdf@gmail.com>
2020-09-11 23:40:47 +0700

    update notification to handle redirect screen

--- App.js
+++ App.js
@@ -1,58 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/es/integration/react'
 
 import configureStore from './src/store'
 import AppNavigator from './src/navigator/AppNavigator'
-import { fcmService } from './src/services/FCMService'
-import { localNotificationService } from './src/services/LocalNotificationService'
-import Axios from 'axios'
-import { REGISTER_DEVICE } from './src/utils/api'
-import { Platform } from 'react-native'
 
 const { persistor, store } = configureStore()
 
 const App = () => {
-  useEffect(() => {
-    fcmService.registerAppWithFCM
-    fcmService.register(onRegister, onNotification, onOpenNotification)
-    localNotificationService.configure(onOpenNotification)
-
-    function onRegister(token) {
-      console.log('[App] onRegister: ', token)
-      Axios.post(REGISTER_DEVICE, { deviceId: token, type: Platform.OS })
-        .then((res) => {
-          console.log(res.data.data)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-      //alert("[App] onRegister: ", token)
-    }
-
-    function onNotification(notify) {
-      console.log('[App] onNotification: ', notify)
-      //alert("[App] onNotification: ", notify)
-      const options = {
-        soundName: 'default',
-        playSound: true,
-      }
-      localNotificationService.showNotification(0, notify.title, notify.body, notify, options)
-    }
-
-    function onOpenNotification(notify) {
-      console.log('[App] onOpenNotification: ', notify)
-      //alert("Open Notification: " + notify.body)
-    }
-
-    return () => {
-      console.log('[App] unRegister')
-      //alert("[App] unRegister")
-      fcmService.unRegister()
-      localNotificationService.unRegister()
-    }
-  }, [])
-
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
--- src/navigator/AppNavigator.js
+++ src/navigator/AppNavigator.js
@@ -1,41 +1,109 @@
-import React, { useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
-import { connect, useSelector } from 'react-redux';
-import Axios from 'axios';
+import React, { useEffect, useState, useRef } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack'
+import { connect, useSelector } from 'react-redux'
+import Axios from 'axios'
 
-import NewPostNavigator from './NewPostNavigator';
-import TabNavigator from './TabNavigator';
-import LandingNavigator from './LandingNavigator';
-import AuthNavigator from './AuthNavigator';
-import { VERIFY_TOKEN } from '../utils/api';
-import CommentScreen from '../screen/Interaction/CommentScreen';
-import EditProfileScreen from '../screen/Profile/EditProfileScreen';
+import { fcmService } from '../services/FCMService'
+import { localNotificationService } from '../services/LocalNotificationService'
+import { REGISTER_DEVICE } from '../utils/api'
+import { Platform } from 'react-native'
 
-const Stack = createStackNavigator();
+import NewPostNavigator from './NewPostNavigator'
+import TabNavigator from './TabNavigator'
+import LandingNavigator from './LandingNavigator'
+import AuthNavigator from './AuthNavigator'
+import { VERIFY_TOKEN } from '../utils/api'
+import CommentScreen from '../screen/Interaction/CommentScreen'
+import EditProfileScreen from '../screen/Profile/EditProfileScreen'
+import RoutesName from '../utils/RoutesName'
+
+const Stack = createStackNavigator()
 
 const AppNavigator = ({ isLoggedIn }) => {
-  const token = useSelector((state) => state.user.token);
-  const [tokenLoaded, setTokenLoaded] = useState(false);
+  const navRef = useRef()
+  const token = useSelector((state) => state.user.token)
+  const [ready, setReady] = useState(false)
+  const [tokenLoaded, setTokenLoaded] = useState(false)
+  const [onOpenScreen, setOnOpenScreen] = useState(null)
+
+  useEffect(() => {
+    fcmService.registerAppWithFCM
+    fcmService.register(onRegister, onNotification, onOpenNotification)
+    localNotificationService.configure(onOpenNotification)
+
+    function onRegister(token) {
+      console.log('[App] onRegister: ', token)
+      Axios.post(REGISTER_DEVICE, { deviceId: token, type: Platform.OS })
+        .then((res) => {
+          console.log(res.data.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      //alert("[App] onRegister: ", token)
+    }
+
+    function onNotification(notify) {
+      console.log('[App] onNotification: ', notify)
+      //alert("[App] onNotification: ", notify)
+      const options = {
+        soundName: 'default',
+        playSound: true,
+      }
+      localNotificationService.showNotification(0, notify.notification.title, notify.notification.body, notify, options)
+    }
+
+    function onOpenNotification(notify) {
+      console.log('[App] onOpenNotification: ', notify)
+      if (notify.data) {
+        if (notify.data.screen === 'walletHistory') {
+          setOnOpenScreen(RoutesName.WalletTab)
+        }
+      }
+      //alert("Open Notification: " + notify.body)
+    }
+
+    return () => {
+      console.log('[App] unRegister')
+      //alert("[App] unRegister")
+      fcmService.unRegister()
+      localNotificationService.unRegister()
+    }
+  }, [])
 
   useEffect(() => {
     if (token) {
-      Axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
-      console.log(token);
+      Axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+      console.log(token)
       Axios.post(VERIFY_TOKEN)
         .then(() => setTokenLoaded(true))
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err))
     } else {
-      setTokenLoaded(true);
+      setTokenLoaded(true)
     }
-  }, [token]);
+  }, [token])
+
+  useEffect(() => {
+    if (ready && onOpenScreen) {
+      // check push notification
+      console.log(`[App] Navigate to ${onOpenScreen}`)
+      navRef.current.navigate(onOpenScreen)
+      setOnOpenScreen(null)
+    }
+  }, [ready, onOpenScreen])
 
   if (!tokenLoaded) {
-    return null;
+    return null
   }
 
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navRef}
+      onReady={() => {
+        setReady(true)
+      }}
+    >
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {!isLoggedIn ? (
           <>
@@ -43,41 +111,41 @@ const AppNavigator = ({ isLoggedIn }) => {
             <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
           </>
         ) : (
-          <>
-            <Stack.Screen name="TabNavigator" component={TabNavigator} />
-            <Stack.Screen
-              name="New Post"
-              component={NewPostNavigator}
-              options={{
-                cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
-                gestureEnabled: false,
-              }}
-            />
-            <Stack.Screen
-              name="Comment"
-              component={CommentScreen}
-              options={{
-                cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
-                gestureDirection: 'vertical',
-              }}
-            />
-            <Stack.Screen
-              name="EditProfile"
-              component={EditProfileScreen}
-              options={{
-                cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
-                gestureDirection: 'vertical',
-              }}
-            />
-          </>
-        )}
+            <>
+              <Stack.Screen name="TabNavigator" component={TabNavigator} />
+              <Stack.Screen
+                name="New Post"
+                component={NewPostNavigator}
+                options={{
+                  cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+                  gestureEnabled: false,
+                }}
+              />
+              <Stack.Screen
+                name="Comment"
+                component={CommentScreen}
+                options={{
+                  cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+                  gestureDirection: 'vertical',
+                }}
+              />
+              <Stack.Screen
+                name="EditProfile"
+                component={EditProfileScreen}
+                options={{
+                  cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+                  gestureDirection: 'vertical',
+                }}
+              />
+            </>
+          )}
       </Stack.Navigator>
     </NavigationContainer>
-  );
-};
+  )
+}
 
 const mapStateToProps = (state) => ({
   isLoggedIn: state.user.isLoggedIn,
-});
+})
 
-export default connect(mapStateToProps)(AppNavigator);
+export default connect(mapStateToProps)(AppNavigator)

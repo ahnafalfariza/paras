29b5aba6ee8b9280fc4d6a02d58667c01118128b
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-09-16 11:32:25 +0700

    check for ios and styles enhancement

--- src/component/Common/Web.js
+++ src/component/Common/Web.js
@@ -2,14 +2,7 @@ import React from 'react';
 import { WebView } from 'react-native-webview';
 
 const Web = ({ url }) => {
-  return (
-    <WebView
-      source={{ uri: url }}
-      style={{
-        height: 1000,
-      }}
-    />
-  );
+  return <WebView source={{ uri: url }} style={{ flex: 1 }} />;
 };
 
 export default Web;
